f5dfa2e5bf114980d542ba3107b992aaf884a5d8
Derived <ytdli08@gmail.com>
2026-04-25 10:05:22 -0400

    update

--- client/src/event_types/user.ts
+++ client/src/event_types/user.ts
@@ -1,10 +1,11 @@
 export type UserRole = 'user' | 'admin';
 export type UserTier = 'free' | 'premium';
 
+// User interface contains the info needed, such as username and tier status
 export interface User {
   id: string;
   username: string;
-  email: string;
+  email: string; // Not used
   role: UserRole;
   tier: UserTier;
   createdAt: string;
